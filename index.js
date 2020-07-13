ad104bc59b574d1e5af253fcda31117de9e91b3c
wufengliang <44823912@qq.com>
2020-07-13 17:58:49 +0800

    feat: 代码编写

--- index.js
+++ index.js
@@ -1,21 +1,3 @@
-#### Promise A+ 规范
-
-##### 定义我们的YPromise的，采用ES6写法
-
-```
-var p = new Promise((resolve,reject)=>{
-
-})
-```
-
-##### 分析
-
-    1.promise传入的参数必须为函数
-    2.promise传入的参数为函数且该函数内为2个函数(resolve,reject)为promise内处理函数
-
-#### 代码
-
-```
 class YPromise {
     constructor(exector) {
         this.value = null;
@@ -51,5 +33,3 @@ class YPromise {
 YPromise.PENDING = 'pending';
 YPromise.FULFILLED = 'fulfilled';
 YPromise.REJECTED = 'rejected';
-```
-
